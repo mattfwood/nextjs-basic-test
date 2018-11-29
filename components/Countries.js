7bdbe9bc2099e1852a4ce89d5bf51217ba60944a
Matthew Wood <matthewwood@Matthews-MacBook-Pro.local>
2018-11-29 16:26:32 -0600

    init

---
+++ components/Countries.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const ALL_TEAMS = gql`
+  query {
+    teams {
+      id
+      name
+    }
+  }
+`;
+
+
+const Countries = () => (
+  <Query query={ALL_TEAMS}>
+    {({ data, loading, error }) => {
+      console.log({ data, loading, error });
+      if (loading) return <div>Loading...</div>
+      return (
+        <div>{JSON.stringify(data)}</div>
+      )
+    }}
+  </Query>
+)
+
+export default Countries;
